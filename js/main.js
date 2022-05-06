a78dab37ebad39eb18331a090de5f680a715b8de
lucalm2004 <lucalm2004@gmail.com>
2022-05-06 13:08:27 +0200

    versioncasifinal

--- js/main.js
+++ js/main.js
@@ -23,3 +23,14 @@ sr.reveal('.inicio-texto',{delay:200, origin:'top'})
 sr.reveal('.imagenini',{delay:200, origin:'top'})
 sr.reveal('.sobremi, .quese, .contact, .experiencias',{delay:200, origin:'top'})
 
+var check=document.querySelector('.check');
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if (id==true){
+        location.href="/en/index.html"
+    }else{
+        location.href="../en/index.html"
+    }
+    }
